5f10d0f9be6966a8b8d906850871c0864135a102
Gabriel Masson <g.masson010@gmail.com>
2021-01-21 09:49:19 -0300

    feat: Adds app and 404 pages

---
+++ src/assets/images/index.ts
@@ -0,0 +1,26 @@
+// PNG
+export { default as imageFour } from './image4.png';
+
+// SVG
+export { default as imageOne } from './image1.svg';
+export { default as imageTwo } from './image2.svg';
+export { default as imageThree } from './image3.svg';
+export { default as logoImg } from './logo.svg';
+export { default as sparkles } from './sparkles.svg';
+export { default as robot } from './robot.svg';
+
+// SVG COMPONENTS
+export { default as Balloons } from '../images/Ballons';
+export { default as Ship } from '../images/Ship';
+export { default as Island } from '../images/Island';
+export { default as CloudOne } from '../images/CloudOne';
+export { default as CloudTwo } from '../images/CloudTwo';
+export { default as CloudThree } from '../images/CloudThree';
+export { default as CloudFour } from '../images/CloudFour';
+export { default as CloudFive } from '../images/CloudFive';
+export { default as CloudSeven } from '../images/CloudSeven';
+export { default as CloudEight } from '../images/CloudEight';
+export { default as CloudNine } from '../images/CloudNine';
+export { default as HeroBuildings } from '../images/HeroBuildings';
+export { default as HeroForegroundLeft } from '../images/HeroForegroundLeft';
+export { default as HeroForegroundRight } from '../images/HeroForegroundRight';
